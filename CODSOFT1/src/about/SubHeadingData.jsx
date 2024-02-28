9ca985a1e3fafad1b3c19c98ec01d7bde11cc157
Sahil Sharma <sharma2003sahil8657@gmail.com>
2024-02-28 15:41:50 +0530

    TASK 1 First Commit

---
+++ CODSOFT1/src/about/SubHeadingData.jsx
@@ -0,0 +1,54 @@
+// import React from 'react'
+
+// const SubHeadingData = () => {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default SubHeadingData
+const SubHeadingData= {
+    1:[{
+        title: 'About Me',
+        content:(
+            <>
+            <p>
+            I'm a 3rd-year computer engineering student specializing in web development with a strong foundation in React, Data Structures and Algorithms, Computer Fundamentals, and Object-Oriented Programming. My technical expertise combined with excellent communication and collaboration skills allows me to thrive in a team-oriented environment. Eager to continuously learn and grow, I am dedicated to making a meaningful impact in the field of computer engineering.
+             </p>
+            </>
+        )
+    }],
+   
+    2:[{
+        title:'Education',
+        content: (
+            <>
+             <p>
+                Pune Institute of Computer Technology<br></br>
+                Dec 2021 - May 2025
+            </p>
+            <p>
+                S. S. H Junior College, Navi Mumbai<br></br>
+                Percentage: 88.33% (XII 2021)
+            </p>
+            <p>
+                Ryan International School, Navi Mumbai<br></br>
+                Percentage: 91.8% (X 2019)
+            </p>
+            </>
+        )
+    }],
+    3:[{
+        title:'Coming Soon',
+        content: (
+            <>
+           
+            </>
+        )
+    }]
+    
+}
+
+export default SubHeadingData
